e8e25fd15a3eb4a7e8ed9ac92497ed5d7acea06e
Albert Margaryan <66197687+AlbertMargaryan@users.noreply.github.com>
2024-02-08 19:12:01 -0500

    init (✨) Scrolling into sections done

---
+++ src/index.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.scrollTo(0,0);
+});
+
+function handleScrolling(sectionClasses) {
+    let currentSectionIndex = 0;
+    let isScrollBlocked = false;
+
+    function blockScroll() {
+        isScrollBlocked = true;
+    }
+
+    function unblockScroll() {
+        isScrollBlocked = false;
+    }
+
+    function handleScroll(event) {
+    	event.preventDefault();
+        if (isScrollBlocked) {
+            return 0;
+        }
+
+        const direction = event.deltaY > 0 ? "down" : "up";
+
+        if (direction === "down") {
+            if (currentSectionIndex < sectionClasses.length - 1) {
+                currentSectionIndex++;
+                scrollToSection(currentSectionIndex);
+            }
+        } else {
+            if (currentSectionIndex > 0) {
+                currentSectionIndex--;
+                scrollToSection(currentSectionIndex);
+            }
+        }
+    }
+
+    function scrollToSection(index) {
+        const section = document.querySelector(`.${sectionClasses[index]}`);
+        section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
+
+    // Add click event listener to each smooth-scroll link
+    smoothScrollLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const newIndex = sectionClasses.indexOf(targetId);
+            currentSectionIndex = newIndex;
+            scrollToSection(currentSectionIndex);
+        });
+    });
+
+    window.addEventListener("wheel", handleScroll, { passive: false });
+
+    return {
+        blockScroll,
+        unblockScroll
+    };
+}
+
+
+const sections = ["about", "showcase", "skills", "contact"];
+
+const scrolling = handleScrolling(sections);
+
+// Block scrolling
+// scrolling.blockScroll();
+
+// Unblock scrolling
+// scrolling.unblockScroll();
